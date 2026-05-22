0630dc63d0f90ed69747d115d5d4386225c41b73
zor1kww <txltedfatality@gmail.com>
2026-05-23 02:56:49 +0800

    Update player.js

--- players/player.js
+++ players/player.js
@@ -1,112 +1,108 @@
 const players = [
 
 {
-    name: "NeoPvP",
-    region: "KZ",
-    device: "PC",
-    tester: "Zor1k",
+name:"NeoPvP",
+region:"KZ",
+device:"PC",
+tester:"Zor1k",
 
-    mainOverall: 640,
-    subOverall: 470,
+mainOverall:920,
+subOverall:650,
 
-    main: {
-        Hardcore: "HT2",
-        SMP: "HT2",
-        UHC: "LT1",
-        Lite: "HT3",
-        Emerald: "HT2",
-        Diamond: "HT2"
-    },
+main:{
+Hardcore:"HT1",
+SMP:"HT1",
+UHC:"LT1",
+Lite:"HT2",
+Emerald:"HT1",
+Diamond:"HT2"
+},
 
-    sub: {
-        Combo: "HT2",
-        Crystal: "LT1",
-        Mace: "HT3"
-    }
+sub:{
+Crystal:"HT2",
+Mace:"LT1"
+}
 },
 
 {
-    name: "ReaperX",
-    region: "RU",
-    device: "PH",
-    tester: "Blaze",
+name:"Inferno",
+region:"RU",
+device:"PH",
+tester:"Blaze",
 
-    mainOverall: 620,
-    subOverall: 430,
+mainOverall:870,
+subOverall:620,
 
-    main: {
-        Hardcore: "HT2",
-        Dragonhide: "HT2",
-        Wand: "LT1",
-        Lite: "HT3"
-    },
+main:{
+Dragonhide:"HT1",
+Wand:"HT2",
+EmeraldPot:"HT2"
+},
 
-    sub: {
-        Axe: "HT2",
-        Bed: "LT2"
-    }
+sub:{
+Combo:"HT2",
+Crystal:"HT2"
+}
 },
 
 {
-    name: "DarkBlade",
-    region: "UA",
-    device: "TB",
-    tester: "Inferno",
+name:"ReaperX",
+region:"UA",
+device:"TB",
+tester:"Inferno",
 
-    mainOverall: 590,
-    subOverall: 450,
+mainOverall:790,
+subOverall:570,
 
-    main: {
-        Emerald: "HT2",
-        DragonhideKB: "LT1",
-        SMP: "HT3"
-    },
+main:{
+Hardcore:"HT2",
+Lite:"HT2"
+},
 
-    sub: {
-        Crystal: "HT2",
-        Gapple: "LT1"
-    }
+sub:{
+Mace:"HT2",
+Gapple:"LT1"
+}
 },
 
 {
-    name: "Vortex",
-    region: "UZ",
-    device: "OTG",
-    tester: "NeoPvP",
+name:"DarkBlade",
+region:"UZ",
+device:"OTG",
+tester:"NeoPvP",
 
-    mainOverall: 540,
-    subOverall: 390,
+mainOverall:730,
+subOverall:520,
 
-    main: {
-        Lite: "HT3",
-        UHC: "LT2"
-    },
+main:{
+Diamond:"HT2",
+DragonhideKB:"LT1"
+},
 
-    sub: {
-        Combo: "HT3",
-        DeBuff: "LT1"
-    }
+sub:{
+Axe:"HT2",
+Bed:"LT2"
+}
 },
 
 {
-    name: "Inferno",
-    region: "KG",
-    device: "PC",
-    tester: "ReaperX",
+name:"Blaze",
+region:"KG",
+device:"PC",
+tester:"Zor1k",
 
-    mainOverall: 660,
-    subOverall: 510,
+mainOverall:690,
+subOverall:610,
 
-    main: {
-        Hardcore: "HT1",
-        SMP: "HT2",
-        EmeraldPot: "HT2"
-    },
+main:{
+SMP:"HT2",
+UHC:"LT1"
+},
 
-    sub: {
-        Crystal: "HT2",
-        Axe: "HT2"
-    }
+sub:{
+Crystal:"HT2",
+Combo:"HT2"
+}
 }
 
 ];
