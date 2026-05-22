b416ca189bb8d93eaa6bf3a7c5009e2faea9649f
zor1kww <txltedfatality@gmail.com>
2026-05-23 02:24:30 +0800

    Update player.js

--- players/player.js
+++ players/player.js
@@ -1,1 +1,112 @@
+const players = [
 
+{
+    name: "NeoPvP",
+    region: "KZ",
+    device: "PC",
+    tester: "Zor1k",
+
+    mainOverall: 640,
+    subOverall: 470,
+
+    main: {
+        Hardcore: "HT2",
+        SMP: "HT2",
+        UHC: "LT1",
+        Lite: "HT3",
+        Emerald: "HT2",
+        Diamond: "HT2"
+    },
+
+    sub: {
+        Combo: "HT2",
+        Crystal: "LT1",
+        Mace: "HT3"
+    }
+},
+
+{
+    name: "ReaperX",
+    region: "RU",
+    device: "PH",
+    tester: "Blaze",
+
+    mainOverall: 620,
+    subOverall: 430,
+
+    main: {
+        Hardcore: "HT2",
+        Dragonhide: "HT2",
+        Wand: "LT1",
+        Lite: "HT3"
+    },
+
+    sub: {
+        Axe: "HT2",
+        Bed: "LT2"
+    }
+},
+
+{
+    name: "DarkBlade",
+    region: "UA",
+    device: "TB",
+    tester: "Inferno",
+
+    mainOverall: 590,
+    subOverall: 450,
+
+    main: {
+        Emerald: "HT2",
+        DragonhideKB: "LT1",
+        SMP: "HT3"
+    },
+
+    sub: {
+        Crystal: "HT2",
+        Gapple: "LT1"
+    }
+},
+
+{
+    name: "Vortex",
+    region: "UZ",
+    device: "OTG",
+    tester: "NeoPvP",
+
+    mainOverall: 540,
+    subOverall: 390,
+
+    main: {
+        Lite: "HT3",
+        UHC: "LT2"
+    },
+
+    sub: {
+        Combo: "HT3",
+        DeBuff: "LT1"
+    }
+},
+
+{
+    name: "Inferno",
+    region: "KG",
+    device: "PC",
+    tester: "ReaperX",
+
+    mainOverall: 660,
+    subOverall: 510,
+
+    main: {
+        Hardcore: "HT1",
+        SMP: "HT2",
+        EmeraldPot: "HT2"
+    },
+
+    sub: {
+        Crystal: "HT2",
+        Axe: "HT2"
+    }
+}
+
+];
