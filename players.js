6686835cf4a839db05023aa502ae18536266a041
zor1kww <txltedfatality@gmail.com>
2026-05-30 21:38:34 +0800

    Update players.js

--- players.js
+++ players.js
@@ -1,43 +1,20 @@
-// База данных игроков MultiCraftCISTiers
 const players = [
     {
         name: "-999-",
         region: "RU",
         device: "PC",
-        tiers: {
-            "Hardcore": "HT1",
-            "Manhunt": "LT1",
-            "Diamond": "HT2",
-            "Beast": "LT2",
-            "Emerald": "HT3",
-            "Emerald Pot": "LT3",
-            "RVM": "HT4",
-            "Dragonhide": "LT4",
-            "Pickaxe": "HT5",
-            "Combo": "LT5",
-            "Gapple": "HT1",
-            "Mace": "Unranked",
-            "Crystal": "Unranked"
-        }
+        tiers: { "Hardcore": "HT1", "Manhunt": "LT1", "Diamond": "HT2", "Beast": "LT2", "Emerald": "HT3", "Emerald Pot": "LT3", "RVM": "HT4", "Dragonhide": "LT4", "Pickaxe": "HT5", "Combo": "LT5", "Gapple": "HT1" }
     },
     {
         name: "zor1kkqwix",
         region: "RU",
         device: "PC",
-        tiers: {
-            "Hardcore": "HT1",
-            "Manhunt": "HT1",
-            "Diamond": "HT1",
-            "Beast": "HT1",
-            "Emerald": "HT1",
-            "Emerald Pot": "HT1",
-            "RVM": "HT1",
-            "Dragonhide": "HT1",
-            "Pickaxe": "HT1",
-            "Combo": "HT1",
-            "Gapple": "HT1",
-            "Mace": "HT1",
-            "Crystal": "HT1"
-        }
+        tiers: { "Hardcore": "HT1", "Manhunt": "HT1", "Diamond": "HT1", "Beast": "HT1", "Emerald": "HT1", "Emerald Pot": "HT1", "RVM": "HT1", "Dragonhide": "HT1", "Pickaxe": "HT1", "Combo": "HT1", "Gapple": "HT1", "Mace": "HT1", "Crystal": "HT1" }
+    },
+    {
+        name: "Sneger",
+        region: "RU",
+        device: "PC",
+        tiers: { "Hardcore": "LT2", "Manhunt": "HT3", "Diamond": "LT3" }
     }
 ];
