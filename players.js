7fc53f1d12009a6db91afd05e45e8b518ca91dfe
zor1kww <txltedfatality@gmail.com>
2026-06-11 15:54:36 +0300

    Update players.js

--- players.js
+++ players.js
@@ -1,60 +1,23 @@
-// players.js
 const players = [
-  {
-    name: "zor1kkqwix",
-    region: "RU",
-    device: "PC",
-    tiers: {
-      Hardcore: "HT1",
-      Manhunt: "HT1",
-      Diamond: "HT1",
-      Beast: "HT1",
-      Emerald: "HT1",
-      "Emerald Pot": "HT1",
-      RVM: "HT1",
-      Dragonhide: "HT1",
-      Pickaxe: "HT1"
+    {
+        name: "OldSchool_PVP",
+        region: "BY",
+        device: "TB",
+        retired: true,
+        tiers: {
+            "Hardcore": "LT1",
+            "Manhunt": "HT2",
+            "Diamond": "Unranked"
+        }
+    },
+    {
+        name: "zor1kkqwix",
+        region: "RU",
+        device: "PC",
+        retired: false,
+        tiers: {
+            "Hardcore": "HT1",
+            "Manhunt": "HT1"
+        }
     }
-  },
-  {
-    name: "-999-",
-    region: "RU",
-    device: "PC",
-    tiers: {
-      Hardcore: "HT1",
-      Manhunt: "LT1",
-      Diamond: "HT2",
-      Beast: "LT2"
-    }
-  },
-  {
-    name: "Sneger",
-    region: "UA",
-    device: "PH",
-    tiers: {
-      Hardcore: "LT1",
-      Manhunt: "HT2",
-      Diamond: "LT2",
-      Combo: "HT1"
-    }
-  },
-  {
-    name: "OldSchool_PVP",
-    region: "BY",
-    device: "TB",
-    retired: true,
-    tiers: {
-      Hardcore: "HT2",
-      Manhunt: "LT2"
-    }
-  },
-  {
-    name: "Nomad_Warrior",
-    region: "KZ",
-    device: "CT",
-    retired: true,
-    tiers: {
-      Hardcore: "LT3"
-    }
-  }
 ];
