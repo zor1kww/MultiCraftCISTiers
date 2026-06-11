5ce16b1ad706ba9ae2e246e964f10c0a4d436a71
zor1kww <txltedfatality@gmail.com>
2026-06-11 21:52:35 +0300

    Update players.js

--- players/players.js
+++ players/players.js
@@ -1,122 +1,45 @@
-// Официальная база данных игроков MultiCraftCISTiers
 const players = [
+    {
+        name: "Sneger",
+        region: "RU",
+        device: "PH",
+        retired: true,
+        tiers: {
+            "Dragonhide": "HT1",
+            "Emerald": "LT1",
+            "Diamond": "HT1",
+            "RVM": "HT1",
+            "Crystal": "HT2",
+            "Combo": "LT1",
+            "Mace": "HT1",
+            "Hardcore": "LT1",
+            "SMP": "LT1"
+        }
+    },
     {
         name: "-999-",
         region: "RU",
-        device: "PC",
-        retired: false,
+        device: "TB",
+        retired: true,
         tiers: {
-            "Hardcore": "HT1",
-            "Manhunt": "HT1",
-            "Diamond": "HT2",
-            "Beast": "HT1",
+            "Dragonhide": "LT1",
             "Emerald": "HT1",
-            "Emerald Pot": "LT1",
+            "Diamond": "LT1",
             "RVM": "HT1",
-            "Dragonhide": "HT2",
-            "Pickaxe": "LT1",
+            "Crystal": "HT1",
             "Combo": "HT1",
-            "Gapple": "HT1",
-            "Mace": "HT2",
-            "Crystal": "LT1"
+            "Mace": "HT1",
+            "Hardcore": "HT1",
+            "SMP": "HT1"
         }
     },
     {
         name: "zor1kkqwix",
         region: "RU",
-        device: "PC",
-        retired: false,
-        tiers: {
-            "Hardcore": "HT1",
-            "Manhunt": "LT1",
-            "Diamond": "HT1",
-            "Beast": "HT2",
-            "Emerald": "HT2",
-            "Emerald Pot": "HT1",
-            "RVM": "LT1",
-            "Dragonhide": "HT1",
-            "Pickaxe": "HT2",
-            "Combo": "LT1",
-            "Gapple": "HT1",
-            "Mace": "LT1",
-            "Crystal": "HT1"
-        }
-    },
-    {
-        name: "Sneger",
-        region: "BY",
-        device: "PC",
-        retired: false,
-        tiers: {
-            "Hardcore": "LT1",
-            "Manhunt": "HT2",
-            "Diamond": "LT2",
-            "Beast": "HT2",
-            "Emerald": "LT1",
-            "Emerald Pot": "LT2",
-            "RVM": "HT2",
-            "Dragonhide": "LT1",
-            "Pickaxe": "LT2",
-            "Combo": "HT2",
-            "Gapple": "LT1",
-            "Mace": "HT3",
-            "Crystal": "LT2"
-        }
-    },
-    {
-        name: "FixEye_PvP",
-        region: "UA",
         device: "PH",
-        retired: false,
-        tiers: {
-            "Hardcore": "HT3",
-            "Manhunt": "LT3",
-            "Diamond": "HT4",
-            "Beast": "Unranked",
-            "Emerald": "LT4",
-            "Emerald Pot": "HT5",
-            "RVM": "Unranked",
-            "Dragonhide": "LT5",
-            "Pickaxe": "Unranked",
-            "Combo": "HT3",
-            "Gapple": "Unranked"
-        }
-    },
-    {
-        name: "C CIS_Warrior",
-        region: "KZ",
-        device: "TB",
         retired: true,
         tiers: {
-            "Hardcore": "LT2",
-            "Manhunt": "HT3",
-            "Diamond": "LT3",
-            "Beast": "HT4",
-            "Emerald": "Unranked",
-            "Emerald Pot": "Unranked",
-            "RVM": "LT4",
-            "Dragonhide": "Unranked",
-            "Pickaxe": "Unranked",
-            "Combo": "LT2",
-            "Crystal": "HT4"
-        }
-    },
-    {
-        name: "MinecraftPro",
-        region: "UZ",
-        device: "CT",
-        retired: false,
-        tiers: {
-            "Hardcore": "LT4",
-            "Manhunt": "HT5",
-            "Diamond": "LT5",
-            "Beast": "Unranked",
-            "Emerald": "Unranked",
-            "Emerald Pot": "Unranked",
-            "RVM": "Unranked",
-            "Dragonhide": "Unranked",
-            "Pickaxe": "Unranked",
-            "Mace": "LT4"
+            "Hardcore": "LT1"
         }
     }
 ];
