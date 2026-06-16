cbaad18f5848bcf5353ef0309331a6e2b9fd3cd7
zor1kww <txltedfatality@gmail.com>
2026-06-16 17:55:13 +0800

    Update players.js

--- players/players.js
+++ players/players.js
@@ -39,5 +39,66 @@ const players = [
         tiers: {
             "Hardcore": "LT1"
         }
+    },
+    {
+        name: "VortexFx",
+        region: "BY",
+        device: "PH",
+        tiers: {
+            "Dragonhide": "LT1",
+            "Emerald": "HT2",
+            "Diamond": "LT2",
+            "Crystal": "HT1",
+            "Combo": "LT1",
+            "Mace": "LT1",
+            "Hardcore": "HT1"
+        }
+    },
+    {
+        name: "Drakon_CS",
+        region: "KZ",
+        device: "PH",
+        tiers: {
+            "Diamond": "HT1",
+            "RVM": "LT1",
+            "Mace": "HT2",
+            "Hardcore": "LT2"
+        }
+    },
+    {
+        name: "Xenon_ZT",
+        region: "RU",
+        device: "TB",
+        tiers: {
+            "Dragonhide": "HT2",
+            "Emerald": "HT1",
+            "Crystal": "LT1",
+            "Combo": "HT1",
+            "SMP": "LT1"
+        }
+    },
+    {
+        name: "FluffyCat",
+        region: "UA",
+        device: "PC",
+        tiers: {
+            "RVM": "LT1",
+            "Crystal": "HT2",
+            "Combo": "LT2",
+            "SMP": "HT1"
+        }
+    },
+    {
+        name: "LEGENDAMERRO",
+        region: "RU",
+        device: "PH",
+        tiers: {
+            "Dragonhide": "LT5",
+            "Emerald": "LT5",
+            "Diamond": "LT5",
+            "RVM": "LT5",
+            "Mace": "LT5",
+            "Hardcore": "LT5"
+        }
     }
 ];
