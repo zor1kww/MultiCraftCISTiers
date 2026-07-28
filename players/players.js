529eb9d332aa02a1ead2e3dea933f882d83ca924
zor1kww <txltedfatality@gmail.com>
2026-07-28 17:13:28 +0800

    auto update tier: Straight -> Hardcore (LT3)

--- players/players.js
+++ players/players.js
@@ -838,7 +838,7 @@ const players = [
             Dragonhide: "HT4",
             "Diamond Pot": "LT4",
             SMP: "HT5",
-            Hardcore: "LT4",
+            Hardcore: "LT3",
             Emerald: "LT3",
             Crystal: "LT3",
             Beast: "LT4",
