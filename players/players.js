546dc277fcacbe4fa7b7d305d2b78248a3bcb2b7
zor1kww <txltedfatality@gmail.com>
2026-09-20 22:08:09 +0800

    result: Prosto_oleg100-7 -> Emerald (HT2)

--- players/players.js
+++ players/players.js
@@ -2306,8 +2306,8 @@ const players = [
                 retired: false
             },
             Emerald: {
-                tier: "LT2",
-                date: "2026-08-25",
+                tier: "HT2",
+                date: "2026-09-20",
                 retired: false
             },
             Dragonhide: {
@@ -2599,11 +2599,22 @@ const players = [
                 scoreOpponent: 3,
                 winner: "player",
                 comment: null
+            },
+            {
+                date: "2026-09-20",
+                kit: "Emerald",
+                opponent: "DzIla_EDITSmob",
+                tierBefore: "Unranked",
+                tierAfter: "HT2",
+                scorePlayer: 3,
+                scoreOpponent: 6,
+                winner: "opponent",
+                comment: null
             }
         ],
         penaltyByKit: {
             Emerald: {
-                points: 0,
+                points: 0.8,
                 firstPenaltyDate: "2026-08-24"
             },
             "Diamond Pot": {
@@ -3709,6 +3720,17 @@ const players = [
                 scorePlayer: 3,
                 scoreOpponent: 4,
                 winner: "opponent",
+                comment: null
+            },
+            {
+                date: "2026-09-20",
+                kit: "Emerald",
+                opponent: "Prosto_oleg100-7",
+                tierBefore: "LT1",
+                tierAfter: "LT1",
+                scorePlayer: 6,
+                scoreOpponent: 3,
+                winner: "player",
                 comment: null
             }
         ],
