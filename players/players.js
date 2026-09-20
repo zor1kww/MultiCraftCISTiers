3648a56225c9e93e369d1b06bcc945b2d2721a2c
zor1kww <txltedfatality@gmail.com>
2026-09-20 23:47:17 +0800

    Админ-панель: удалена запись лога дуэлей у DzIla_EDITSmob (Emerald, 2026-09-20) + парная запись оппонента

--- players/players.js
+++ players/players.js
@@ -2647,17 +2647,6 @@ const players = [
             {
                 date: "2026-09-20",
                 kit: "Emerald",
-                opponent: "DzIla_EDITSmob",
-                tierBefore: "Unranked",
-                tierAfter: "HT2",
-                scorePlayer: 3,
-                scoreOpponent: 6,
-                winner: "opponent",
-                comment: null
-            },
-            {
-                date: "2026-09-20",
-                kit: "Emerald",
                 opponent: "система",
                 tierBefore: "HT2",
                 tierAfter: "LT2",
@@ -3845,17 +3834,6 @@ const players = [
                 scorePlayer: 3,
                 scoreOpponent: 6,
                 winner: "opponent",
-                comment: null
-            },
-            {
-                date: "2026-09-20",
-                kit: "Emerald",
-                opponent: "Prosto_oleg100-7",
-                tierBefore: "LT1",
-                tierAfter: "LT1",
-                scorePlayer: 6,
-                scoreOpponent: 3,
-                winner: "player",
                 comment: null
             },
             {
