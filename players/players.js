55402eaec48ab6ee0e4e76f35ea54fde1723f5ae
zor1kww <txltedfatality@gmail.com>
2026-09-21 10:33:01 +0800

    result: DzIla_EDITSmob -> Dragonhide (LT1)

--- players/players.js
+++ players/players.js
@@ -2687,6 +2687,17 @@ const players = [
                 scoreOpponent: 5,
                 winner: "player",
                 comment: null
+            },
+            {
+                date: "2026-09-21",
+                kit: "Dragonhide",
+                opponent: "DzIla_EDITSmob",
+                tierBefore: "LT1",
+                tierAfter: "LT1",
+                scorePlayer: 6,
+                scoreOpponent: 5,
+                winner: "player",
+                comment: null
             }
         ],
         penaltyByKit: {
@@ -3834,6 +3845,17 @@ const players = [
                 scorePlayer: 6,
                 scoreOpponent: 3,
                 winner: "player",
+                comment: null
+            },
+            {
+                date: "2026-09-21",
+                kit: "Dragonhide",
+                opponent: "Prosto_oleg100-7",
+                tierBefore: "HT2",
+                tierAfter: "LT1",
+                scorePlayer: 5,
+                scoreOpponent: 6,
+                winner: "opponent",
                 comment: null
             },
             {
