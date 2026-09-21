26fca8de0df794f63c2af00b23d1a9da4825d28e
zor1kww <txltedfatality@gmail.com>
2026-09-21 10:41:24 +0800

    Админ-панель: удалена запись лога дуэлей у DzIla_EDITSmob (Dragonhide, 2026-09-21) + парная запись оппонента

--- players/players.js
+++ players/players.js
@@ -2665,17 +2665,6 @@ const players = [
                 scoreOpponent: 5,
                 winner: "player",
                 comment: null
-            },
-            {
-                date: "2026-09-21",
-                kit: "Dragonhide",
-                opponent: "DzIla_EDITSmob",
-                tierBefore: "LT1",
-                tierAfter: "LT1",
-                scorePlayer: 6,
-                scoreOpponent: 5,
-                winner: "player",
-                comment: null
             }
         ],
         penaltyByKit: {
@@ -3823,17 +3812,6 @@ const players = [
                 scorePlayer: 6,
                 scoreOpponent: 3,
                 winner: "player",
-                comment: null
-            },
-            {
-                date: "2026-09-21",
-                kit: "Dragonhide",
-                opponent: "Prosto_oleg100-7",
-                tierBefore: "HT2",
-                tierAfter: "LT1",
-                scorePlayer: 5,
-                scoreOpponent: 6,
-                winner: "opponent",
                 comment: null
             },
             {
