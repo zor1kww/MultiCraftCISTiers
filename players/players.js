0485407a2d5581f7f12a60b6ae979990ea2c2bb8
zor1kww <txltedfatality@gmail.com>
2026-09-22 02:12:56 +0800

    result: Grey -> Emerald Pot (HT3)

--- players/players.js
+++ players/players.js
@@ -3923,6 +3923,17 @@ const players = [
                 scoreOpponent: 0,
                 winner: "player",
                 comment: "Хз едтте Сами решайте оставить или нед"
+            },
+            {
+                date: "2026-09-21",
+                kit: "Emerald Pot",
+                opponent: "Grey",
+                tierBefore: "LT1",
+                tierAfter: "LT1",
+                scorePlayer: 4,
+                scoreOpponent: 0,
+                winner: "player",
+                comment: "Хз едтте Сами решайте оставить или нед"
             }
         ],
         penaltyByKit: {
@@ -5238,8 +5249,36 @@ const players = [
     },
     {
         name: "Grey",
-        tiers: {},
+        tiers: {
+            "Emerald Pot": {
+                tier: "LT3",
+                date: "2026-09-21",
+                retired: false
+            }
+        },
         matchHistory: [
+            {
+                date: "2026-09-21",
+                kit: "Emerald Pot",
+                opponent: "DzIla_EDITSmob",
+                tierBefore: "Unranked",
+                tierAfter: "HT3",
+                scorePlayer: 0,
+                scoreOpponent: 4,
+                winner: "opponent",
+                comment: "Хз едтте Сами решайте оставить или нед"
+            },
+            {
+                date: "2026-09-21",
+                kit: "Emerald Pot",
+                opponent: "система",
+                tierBefore: "HT3",
+                tierAfter: "LT3",
+                scorePlayer: 0,
+                scoreOpponent: 0,
+                winner: "opponent",
+                comment: "Автопонижение: накоплено 2 штрафных очка"
+            },
             {
                 date: "2026-09-21",
                 kit: "Emerald Pot",
@@ -5263,6 +5302,12 @@ const players = [
                 comment: "Автопонижение: накоплено 2 штрафных очка"
             }
         ],
-        penaltyByKit: {}
+        penaltyByKit: {
+            "Emerald Pot": {
+                points: 0.0,
+                firstPenaltyDate: "2026-09-21"
+            }
+        },
+        region: "RU"
     }
 ];
